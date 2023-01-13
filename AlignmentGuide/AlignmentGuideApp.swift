a56d01f616a91a7c217bae21c590121215e541e1
TAAT <20purpleblue10@gmail.com>
2023-01-13 21:44:45 +0900

    Initial Commit

---
+++ AlignmentGuide/AlignmentGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignmentGuideApp.swift
+//  AlignmentGuide
+//
+//  Created by 董 亜飛 on 2023/01/13.
+//
+
+import SwiftUI
+
+@main
+struct AlignmentGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
